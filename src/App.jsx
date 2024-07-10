6fc132a2a99a8194eceb00595db644f444097c19
ensarsnr <dev.anise@outlook>
2024-07-10 14:39:59 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LiveList from "./components/LiveList";
 
 function App() {
   return (
-      <div id="cssportal-grid">
+      <div id="cssportal-grid" className="bg-light">
         <div
           className="rounded bg-light shadow d-flex align-items-center justify-content-center"
           id="div1"
@@ -12,7 +12,7 @@ function App() {
           <div className="row  w-100 h-75">
             <div className="col d-flex align-items-center justify-content-start">
               <div className="ml-2 text-light w-75 text-center ">
-                <span className="2xl:text-8xl xl:text-5xl lg:text-3xl md:3xl text-dark font-bold">Ayşe </span>
+                <span className="2xl:text-8xl xl:text-5xl lg:text-3xl md:3xl text-gray-600 font-bold">Ayşe </span>
               </div>
             </div>
           <div className="col bg-red-300 mr-3 rounded shadow">
@@ -20,13 +20,13 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="rounded bg-warning shadow" id="div2">
+        <div className="rounded bg-light shadow" id="div2">
           div2
         </div>
-        <div className="rounded bg-info shadow" id="div3">
+        <div className="rounded bg-light border shadow" id="div3">
           <div className="row h-[10%] w-100">
             <div className="col-12 text-center flex ml-3 rounded ">
-              <h1 className="text-light m-auto 2xl:text-6xl xl:text-4xl lg:text-2xl font-bold">Anlık Satışlar</h1>
+              <h1 className="text-gray-600 m-auto 2xl:text-6xl xl:text-4xl lg:text-2xl font-bold">Anlık Satışlar</h1>
             </div>
           </div>
           <div className="row h-[90%] w-[100%] m-auto">
