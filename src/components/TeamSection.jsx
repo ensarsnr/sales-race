c4cde25eb1a28e8af688e12245c85611f98159d2
ensarsnr <dev.anise@outlook>
2024-07-12 13:17:48 +0300

    Changed class

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TeamSection() {
   return (
-    <div className="bg-light rounded-3xl shadow 2xl:row-span-2 2xl:col-start-1 xl:col-2 lg:col-span-1 2xl:row-start-5">
+    <div className="bg-light rounded-3xl shadow 2xl:row-span-2 xl:col-2 lg:col-span-1 2xl:row-start-3">
       <div className="h-full w-full">
         <div className="w-full text-center p-5">
           <span className="2xl:text-8xl xl:text-6xl lg:text-3xl font-bold text-dark">
