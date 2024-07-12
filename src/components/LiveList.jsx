8bb5ef448c27e84e5def9921fadd02bcb02f8fc5
ensarsnr <dev.anise@outlook>
2024-07-12 08:17:02 +0300

    Updated

--- src/components/LiveList.jsx
+++ src/components/LiveList.jsx
@@ -4,12 +4,12 @@ import { GiTrophyCup } from "react-icons/gi";
 function LiveList() {
   const salesList = [
     {
-      name: "Ayşe Denem",
-      team: "takım1",
-      sales: "$2000",
+      name: "Eyüp Ensar Şener",
+      team: "Takimİsimleri",
+      sales: "$20000000",
     },
     {
-      name: "Ahmet Den..",
+      name: "Pascal Nouma",
       team: "takım2",
       sales: "$3000",
     },
@@ -62,233 +62,229 @@ function LiveList() {
       name: "Deniz",
       team: "takım2",
       sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
-      {
-        name: "Deniz",
-        team: "takım2",
-        sales: "$5000",
-      },
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
+    {
+      name: "Deniz",
+      team: "takım2",
+      sales: "$5000",
+    },
   ];
 
   return (
     <div className="table-container">
-    <table className="w-full min-w-full shadow divide-y divide-gray-200 rounded-lg">
-      <thead className="bg-white rounded-t-lg">
-        <tr>
-          <th
-            scope="col"
-            className="px-6 py-3 2xl:text-6xl lg:text-xs font-bold text-center text-gray-500 uppercas"
-          >
-            Sıralama
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 2xl:text-6xl lg:text-xs font-bold text-center text-gray-500 uppercas"
-          >
-            İsim
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 2xl:text-6xl lg:text-xs font-bold text-center text-gray-500 uppercas"
-          >
-            Takım
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 2xl:text-6xl lg:text-xs font-bold text-center text-gray-500 uppercas"
-          >
-            Satış
-          </th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-gray-200">
-        {salesList.map((e, index) => (
-          <tr
-            className={
-              index % 2 === 0
-                ? "bg-sky-500"
-                : "bg-white"
-            }
-            key={index}
-          >
-            <td className="py-3 pl-4 text-gray-500 text-center">
-              <div className=" text-lg 2xl:text-7xl xl:text-1xl lg:text-xl sm:text-xs">
-                <div className="m-auto">
-                  {index === 0 ? (
-                    <GiTrophyCup
-                      className="m-auto 2xl:w-32 2xl:h-32 xl:w-9 xl:h-9 lg:w-32 lg:h-32 sm:w-3 sm:h-3 w-10 h-10"
-                      color="gold"
-                    />
-                  ) : index === 1 ? (
-                    <GiTrophyCup
-                      className="m-auto 2xl:w-32 2xl:h-32 xl:w-9 xl:h-9 lg:w-6 lg:h-6 sm:w-3 sm:h-3 w-10 h-10"
-                      color="#aeb0ae"
-                    />
-                  ) : index === 2 ? (
-                    <GiTrophyCup
-                      className="m-auto 2xl:w-32 2xl:h-32 xl:w-9 xl:h-9 lg:w-6 lg:h-6 sm:w-3 sm:h-3 w-10 h-10"
-                      color="#CD7F32"
-                    />
-                  ) : (
-                    index + 1
-                  )}
-                </div>
-              </div>
-            </td>
-            <td className="py-3 pl-4 text-gray-500 text-center text-lg 2xl:text-6xl xl:text-1xl lg:text-4xl sm:text-xs">
-              {e.name}
-            </td>
-            <td className="py-3 pl-4 text-gray-500 text-center text-lg 2xl:text-6xl xl:text-1xl lg:text-xl sm:text-xs">
-              {e.team}
-            </td>
-            <td className="py-3 pl-4 text-gray-500 text-center text-lg 2xl:text-6xl xl:text-1xl lg:text-xl sm:text-xs">
-              {e.sales}
-            </td>
+      <table className="w-full min-w-full shadow divide-y divide-gray-200 rounded-lg">
+        <thead className="bg-white rounded-t-lg">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 2xl:text-5xl lg:text-xs font-bold text-center text-gray-500 uppercas"
+            >
+              NO
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 2xl:text-5xl lg:text-xs font-bold text-center text-gray-500 uppercas"
+            >
+              İsim-Soyisim
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 2xl:text-5xl lg:text-xs font-bold text-center text-gray-500 uppercas"
+            >
+              Takım
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 2xl:text-5xl lg:text-xs font-bold text-center text-gray-500 uppercas"
+            >
+              Satış
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+        </thead>
+        <tbody className="divide-y divide-gray-200">
+          {salesList.map((e, index) => (
+            <tr
+              className={index % 2 === 0 ? "bg-sky-500" : "bg-white"}
+              key={index}
+            >
+              <td className="py-3 pl-4 text-gray-500 text-center">
+                <div className=" text-lg text-black 2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
+                  <div className="m-auto">
+                    {index === 0 ? (
+                      <GiTrophyCup
+                        className="m-auto 2xl:w-20 2xl:h-16 xl:w-9 xl:h-9 lg:w-32 lg:h-32 sm:w-3 sm:h-3 w-10 h-10"
+                        color="gold"
+                      />
+                    ) : index === 1 ? (
+                      <GiTrophyCup
+                        className="m-auto 2xl:w-20 2xl:h-16 xl:w-9 xl:h-9 lg:w-6 lg:h-6 sm:w-3 sm:h-3 w-10 h-10"
+                        color="#aeb0ae"
+                      />
+                    ) : index === 2 ? (
+                      <GiTrophyCup
+                        className="m-auto 2xl:w-20 2xl:h-16 xl:w-10 xl:h-10 lg:w-10 lg:h-10 sm:w-3 sm:h-3 w-10 h-10"
+                        color="#CD7F32"
+                      />
+                    ) : (
+                      index + 1
+                    )}
+                  </div>
+                </div>
+              </td>
+              <td className="py-3 pl-4 text-dark text-center text-lg 2xl:text-5xl xl:text-1xl lg:text-4xl sm:text-xs">
+                {e.name}
+              </td>
+              <td className="py-3 pl-4 text-dark text-center text-lg 2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
+                {e.team}
+              </td>
+              <td className="py-3 pl-4 text-dark text-center text-lg 2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
+                {e.sales}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
