6d9f9fae44910c6943e8cf875bd926f9d641403b
ensarsnr <dev.anise@outlook>
2024-07-12 08:17:17 +0300

    Created comp

---
+++ src/components/LiveSeller.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function LiveSeller() {
+  return (
+    <div className="bg-light rounded-3xl shadow row-span-2">
+      <div className="w-full p-5 text-center">
+        <span className="2xl:text-8xl xl:text-6xl lg:text-5xl font-bold">s</span>
+      </div>
+      <div className="h-4/6">
+        <div className="shadow m-auto w-3/4 rounded-3xl h-full bg-black">
+          {/* Buraya resim gelecek */}s
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LiveSeller;
