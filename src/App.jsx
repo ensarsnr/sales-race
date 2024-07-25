b81526af2610c452679cc82376ae1f65ffe03fc2
ensarsnr <dev.anise@outlook.com>
2024-07-25 09:05:28 +0300

    Added socket io connections and removed unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import LiveList from "./components/LiveList";
 import backgroundImg from "./assets/image/Zemin.png";
 import Navbar from "./components/Navbar";
-import Lottie from "react-lottie-player";
-import graph from "./assets/anims/graph.json";
 import TvComp from "./components/tv/TvComp";
-import party from "./assets/anims/party.json";
 import useSound from "use-sound";
 import sound from "./assets/sound/mixkit-video-game-treasure-2066.wav";
+import services from "./service/index";
+import DesktopComp from "./components/desktop/DesktopComp";
+import io from "socket.io-client";
 
 function App() {
-  const [play] = useSound(sound);
+  const [sales, setSales] = useState([]);
+  const [totalSales, setTotalSales] = useState([]);
+  const [play] = useSound(sound, { volume: 1 });
+  const [isConfetti, setIsConfetti] = useState(false);
+
+  useEffect(() => {
+    // Socket.io bağlantısını kur
+    const socket = io("http://localhost:3000");
+
+    // Sunucudan gelen 'newData' olaylarını dinle
+    socket.on("newData", (newData) => {
+      console.log("Yeni veri alındı:", newData);
+
+      setSales((prevSales) => {
+        const exists = prevSales.some(
+          (sale) =>
+            sale.HIZMET === newData.HIZMET &&
+            sale.TARIH === newData.TARIH &&
+            sale.FIYAT === newData.FIYAT
+        );
+
+        if (!exists) {
+          // Yeni veri eklendiğinde sesi çal
+          play();
+          
+          // Konfeti efektini ayarla ve 2 saniye sonra sıfırla
+          setIsConfetti(true);
+          setTimeout(() => {
+            setIsConfetti(false);
+          }, 3000);
+          
+          return [newData, ...prevSales];
+        }
+
+        return prevSales;
+      });
+    });
+
+    // Sunucudan gelen 'newTotalSales' olaylarını dinle
+    socket.on("newTotalSales", (newTotalSales) => {
+      console.log("Yeni toplam satış verisi alındı:", newTotalSales);
+      setTotalSales(newTotalSales);
+    });
+
+    // Veritabanından başlangıç verilerini al
+    const fetchData = async () => {
+      try {
+        const response = await services.getAllSales();
+        setSales(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchTotalSales = async () => {
+      try {
+        const response = await services.getTotalSales();
+        setTotalSales(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+    fetchTotalSales();
+
+    // Bileşen unmount olduğunda socket bağlantısını kes
+    return () => {
+      socket.disconnect();
+    };
+  }, [play]); // play fonksiyonunu bağımlılıklara ekleyin
 
   return (
     <div
-      onClick={play}
       className="2xl:p-3 xl:p-0 lg:p-0 md:p-0 sm:p-0"
       style={{ backgroundImage: `url(${backgroundImg})` }}
     >
       <Navbar />
-      <TvComp />
-      {/* Laptop-Masaüstü görünümü */}
-      <div className="h-full 2xl:hidden p-3 grid grid-cols-2  gap-3">
-        <div className="bg-light rounded-xl shadow-md p-4">
-          <div style={{ position: "relative" }}>
-            <div
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                height: "100%",
-                width: "100%",
-                zIndex: 10,
-                pointerEvents: "none",
-              }}
-            >
-              <Lottie loop play animationData={party} />
-            </div>
-          </div>
-          <div className="text-center text-3xl">sad</div>
-          <div className="bg-black w-3/4 m-auto shadow-md h-3/4 mt-3 rounded-xl"></div>
-        </div>
-        <div className="bg-light rounded-xl shadow-md p-4">
-          <Lottie play loop animationData={graph} />
-        </div>
-        <div className="bg-light rounded-xl shadow-md p-4">
-          <div className="text-center text-3xl">sad</div>
-          <div className="bg-black w-3/4 m-auto shadow-md h-3/4 mt-3 rounded-xl"></div>
-        </div>
-        <div className="bg-light rounded-xl shadow-md p-4 flex">
-          <div class="block w-full overflow-x-auto">
-            <table class="items-center bg-transparent w-full border-collapse ">
-              <thead>
-                <tr>
-                  <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                    İsim
-                  </th>
-                  <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                    Takım
-                  </th>
-                  <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                    Satış
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
-                    Meryem Yıldız
-                  </th>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                    4,569
-                  </td>
-                  <td class="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    340
-                  </td>
-                </tr>
-                <tr>
-                  <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                    Leyla Kaplan
-                  </th>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    3,985
-                  </td>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    319
-                  </td>
-                </tr>
-                <tr>
-                  <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                    Halil İbrahim Güneş
-                  </th>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    3,513
-                  </td>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    294
-                  </td>
-                </tr>
-                <tr>
-                  <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                    Metin Köse
-                  </th>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    2,050
-                  </td>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    147
-                  </td>
-                </tr>
-                <tr>
-                  <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                    Esra Güler
-                  </th>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    1,795
-                  </td>
-                  <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    190
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="bg-light md:overflow-x-auto  col-span-2 rounded-xl shadow">
-          <LiveList />
-        </div>
-      </div>
+      <TvComp isConfetti={isConfetti} totalSales={totalSales} sales={sales} />
+      <DesktopComp isConfetti={isConfetti} totalSales={totalSales} sales={sales} />
     </div>
   );
 }
