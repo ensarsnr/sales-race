3f1f3f91f56521342169635590c48eb645e4210a
ensarsnr <dev.anise@outlook.com>
2024-07-26 09:11:06 +0300

    Added component data

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ function App() {
       style={{ backgroundImage: `url(${backgroundImg})` }}
     >
       <Navbar />
-      <TvComp isConfetti={isConfetti} totalSales={totalSales} sales={sales} />
+      <TvComp  salesList={sales} />
       <DesktopComp isConfetti={isConfetti} totalSales={totalSales} sales={sales} />
     </div>
   );
