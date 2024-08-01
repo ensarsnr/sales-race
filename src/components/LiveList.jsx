7eb41ccc4a03f7b3cf30f0dd3a615c89dd9a2b5f
ensarsnr <dev.anise@outlook.com>
2024-08-01 12:04:06 +0300

    Added anims

--- src/components/LiveList.jsx
+++ src/components/LiveList.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { GiTrophyCup } from "react-icons/gi";
+import Lottie from "react-lottie-player";
+import goldCup from "../assets/anims/goldcup.json";
+import silverCup from "../assets/anims/silvercup.json";
+import bronzeCup from "../assets/anims/bronzecup.json";
 
 const LiveList = ({ totalSales }) => {
   const sortedSales = totalSales
     .slice()
     .sort((a, b) => b.toplam_satis - a.toplam_satis);
- 
+
   // isim filtreleme
   const normalizeName = (name) => {
     return name.replace(/\s+/g, "").toLowerCase();
   };
- 
+
   return (
     <div className="table-container xl:overflow-x-hidden overflow-x-auto">
       <table className="w-full min-w-full shadow divide-y divide-gray-200 rounded-lg">
@@ -45,26 +49,46 @@ const LiveList = ({ totalSales }) => {
             ) // Murtaza Orçun Başar'ı filtrele
             .map((e, index) => (
               <tr
-                className={index % 2 === 0 ? "bg-sky-500" : "bg-white"}
+                className={
+                  index % 2 === 0
+                    ? "bg-[#00387E] text-light"
+                    : "bg-white text-dark"
+                }
                 key={index}
               >
-                <td className="py-3 pl-4 text-gray-500 text-center">
-                  <div className="text-lg text-black 2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
+                <td
+                  className={
+                    index % 2 === 0
+                      ? "text-light py-3 pl-4 text-center"
+                      : "text-light py-3 pl-4 text-center text-dark"
+                  }
+                >
+                  <div className="text-lg  2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
                     <div className="m-auto">
                       {index === 0 ? (
-                        <GiTrophyCup
-                          className="m-auto 2xl:w-20 2xl:h-16 xl:w-9 xl:h-9 lg:w-32 lg:h-32 sm:w-3 sm:h-3 w-10 h-10"
-                          color="gold"
+                        <Lottie
+                          className="m-auto 2xl:w-32 2xl:h-24 xl:w-9 xl:h-9 lg:w-6 lg:h-6 sm:w-3 sm:h-3 w-10 h-10"
+                          loop
+                          play
+                          animationData={goldCup}
                         />
-                      ) : index === 1 ? (
-                        <GiTrophyCup
-                          className="m-auto 2xl:w-20 2xl:h-16 xl:w-9 xl:h-9 lg:w-6 lg:h-6 sm:w-3 sm:h-3 w-10 h-10"
-                          color="#aeb0ae"
+                      ) : // <GiTrophyCup
+                      //   className="m-auto 2xl:w-20 2xl:h-16 xl:w-9 xl:h-9 lg:w-32 lg:h-32 sm:w-3 sm:h-3 w-10 h-10"
+                      //   color="gold"
+                      // />
+                      index === 1 ? (
+                        <Lottie
+                          className="m-auto 2xl:w-32 2xl:h-24 xl:w-9 xl:h-9 lg:w-6 lg:h-6 sm:w-3 sm:h-3 w-10 h-10"
+                          loop
+                          play
+                          animationData={silverCup}
                         />
                       ) : index === 2 ? (
-                        <GiTrophyCup
-                          className="m-auto 2xl:w-20 2xl:h-16 xl:w-10 xl:h-10 lg:w-10 lg:h-10 sm:w-3 sm:h-3 w-10 h-10"
-                          color="#CD7F32"
+                        <Lottie
+                          className="m-auto 2xl:w-32 2xl:h-24 xl:w-10 xl:h-10 lg:w-10 lg:h-10 sm:w-3 sm:h-3 w-10 h-10"
+                          loop
+                          play
+                          animationData={bronzeCup}
                         />
                       ) : (
                         index + 1
@@ -72,10 +96,10 @@ const LiveList = ({ totalSales }) => {
                     </div>
                   </div>
                 </td>
-                <td className="py-3 pl-4 text-dark text-center text-lg 2xl:text-5xl xl:text-1xl lg:text-4xl sm:text-xs">
+                <td className="py-3 pl-4 text-center text-lg 2xl:text-5xl xl:text-1xl lg:text-4xl sm:text-xs">
                   {e.calisan_adi}
                 </td>
-                <td className="py-3 pl-4 text-dark text-right text-lg 2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
+                <td className="py-3 pl-4 text-right text-lg 2xl:text-5xl xl:text-1xl lg:text-xl sm:text-xs">
                   <span className="mr-10">{e.toplam_satis}$</span>
                 </td>
               </tr>
