5ac4202d5abac7b30f923becf1b4fc1a5afc7c37
ensarsnr <dev.anise@outlook.com>
2024-08-01 15:22:47 +0300

    New anim added

--- src/components/GraphSection.jsx
+++ src/components/GraphSection.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Lottie from "react-lottie-player";
-import graph from "../assets/anims/graph.json";
+import graph from "../assets/anims/lazycat.json";
 function GraphSection() {
   return (
-    <div className="bg-light rounded-3xl shadow row-span-2">
+    <div className="bg-light rounded-3xl shadow row-span-2 p-5">
       <div className="h-full w-full">
+        <h1 style={{fontFamily:"TitleFont"}} className="text-center text-9xl">Ayın Tembeli</h1>
         <Lottie loop animationData={graph} play className="2xl:w-full h-full" />
       </div>
     </div>
